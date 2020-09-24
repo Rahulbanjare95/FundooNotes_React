9629eda631c3e1e1bfa68044e2630d6f66ddef9f
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-09-24 14:02:28 +0530

    refactor(Routes): Add route to forget password

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Register from './Components/Register';
 import './App.css';
 import SignIn from './Components/SignIn';
 import ForgetPassword from './Components/ForgetPassword';
+import ResetPassword from './Components/ResetPassword';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 class App extends Component{
@@ -15,6 +16,7 @@ class App extends Component{
         <Route path="/register" exact component={Register} />
         <Route path="/SignIn" exact component={SignIn} /> 
         <Route path="/ForgetPassword" exact component={ForgetPassword} />
+        <Route path="/ResetPassword" exact component={ResetPassword} />
       </Switch>
       </div>
       </Router>
