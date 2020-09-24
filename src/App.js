3d01ddd05f1fe9e2a3ea72d9a13eeb82e7c93793
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-09-24 12:26:58 +0530

    refactor(Route): adds routing to forget password page from sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Register from './Components/Register';
 import './App.css';
 import SignIn from './Components/SignIn';
+import ForgetPassword from './Components/ForgetPassword';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 class App extends Component{
@@ -13,6 +14,7 @@ class App extends Component{
         <Route path="/" exact component ={SignIn} ></Route>
         <Route path="/register" exact component={Register} />
         <Route path="/SignIn" exact component={SignIn} /> 
+        <Route path="/ForgetPassword" exact component={ForgetPassword} />
       </Switch>
       </div>
       </Router>
