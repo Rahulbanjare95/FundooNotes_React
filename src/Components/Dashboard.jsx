08379f32b2377f8d30dcace3f2594f2e08bca3b7
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-02 06:50:19 +0530

    style(Icons): Add icons on navbar

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap';
+import { Navbar, Button, Form, FormControl } from 'react-bootstrap';
 import { Component } from 'react';
 import download from '../styles/download.jpg';
 import "../styles/Navbar.css";
-
+import { BsArrowClockwise, BsFillGearFill, BsPerson } from "react-icons/bs";
+import SettingsDashboard from './settingsDashboard.jsx';
 class Dashboard extends Component {
 
     render() {
@@ -12,7 +13,26 @@ class Dashboard extends Component {
                 <Navbar.Brand><img
                     src={download}
                     alt="Imge"
-                    className="custom-image"/><span className="custom-brand">FundooNotes</span></Navbar.Brand>
+                    className="custom-image" /><span className="custom-brand">FundooNotes</span></Navbar.Brand>
+
+                <div >
+                    <Form inline >
+                        <FormControl type="text" placeholder="Search" className="searchBar" />
+                        <div className='refresh'>
+                            <Button className='btn-light btn-circle btn-sm'>
+                                <BsArrowClockwise size='27px'> </BsArrowClockwise>
+                            </Button>
+                        </div>
+                        <div className='settings'>
+                            <Button className='btn-light btn-circle btn-sm' onClick={SettingsDashboard}>
+                                <BsFillGearFill size='27px'></BsFillGearFill>
+                            </Button> </div>
+                        <div className='profile'>
+                            <Button className='btn-light btn-circle btn-sm'>
+                                <BsPerson size='27px'></BsPerson>
+                            </Button> </div>
+                    </Form>
+                </div>
             </Navbar>
 
         )
