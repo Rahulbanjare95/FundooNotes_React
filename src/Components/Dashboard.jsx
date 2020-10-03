a002a707b7a64124a4e4dfc076590c225a2596cb
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-03 10:47:49 +0530

    refactor(SideBar): Add onclick to render the sidebar

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,40 +1,74 @@
 import React from "react";
-import { Navbar, Button, Form, FormControl } from 'react-bootstrap';
+import { Navbar, Button, Form, FormControl, DropdownButton, Dropdown } from 'react-bootstrap';
 import { Component } from 'react';
 import download from '../styles/download.jpg';
 import "../styles/Navbar.css";
 import { BsArrowClockwise, BsFillGearFill, BsPerson } from "react-icons/bs";
-import SettingsDashboard from './settingsDashboard.jsx';
+import DrawerToggleButton from "./DrawerToggleButton";
+import SideBar from "./SideBar";
+import BackDrop from "./Backdrop";
+import sideBar from "./SideBar";
+import Backdrop from "./Backdrop";
+
 class Dashboard extends Component {
+    state = {
+        sideDrawerOpen:false
+    };
 
+    drawerToggleClickHandler = () =>{
+        this.setState((previousState) => {
+            return{sideDrawerOpen: !previousState.sideDrawerOpen};
+        });
+    };
     render() {
+        let sideDrawer;
+        let backDrop;
+        if(this.state.sideDrawerOpen){
+            sideDrawer = <SideBar/>;
+            backDrop = <Backdrop />;
+        }
         return (
-            <Navbar variant="default" className="custom-nav-bar" >
-                <Navbar.Brand><img
-                    src={download}
-                    alt="Imge"
-                    className="custom-image" /><span className="custom-brand">FundooNotes</span></Navbar.Brand>
+            <>
+                <Navbar variant="default" className="custom-nav-bar" >
+                    <div>
+                    <DrawerToggleButton click={this.drawerToggleClickHandler}/>
+                    {sideDrawer}
+                    {backDrop}
+                    </div>
+                    <Navbar.Brand><img
+                        src={download}
+                        alt="Imge"
+                        className="custom-image" /><span className="custom-brand">FundooNotes</span></Navbar.Brand>
 
-                <div >
-                    <Form inline >
-                        <FormControl type="text" placeholder="Search" className="searchBar" />
-                        <div className='refresh'>
-                            <Button className='btn-light btn-circle btn-sm'>
-                                <BsArrowClockwise size='27px'> </BsArrowClockwise>
-                            </Button>
-                        </div>
-                        <div className='settings'>
-                            <Button className='btn-light btn-circle btn-sm' onClick={SettingsDashboard}>
-                                <BsFillGearFill size='27px'></BsFillGearFill>
-                            </Button> </div>
-                        <div className='profile'>
-                            <Button className='btn-light btn-circle btn-sm'>
-                                <BsPerson size='27px'></BsPerson>
-                            </Button> </div>
-                    </Form>
-                </div>
-            </Navbar>
-
+                    <div >
+                        <Form inline >
+                            <FormControl type="text" placeholder="Search" className="searchBar" />
+                            <div className='refresh'>
+                                <Button className='btn-light btn-circle btn-sm'>
+                                    <BsArrowClockwise size='27px'> </BsArrowClockwise>
+                                </Button>
+                            </div>
+                            <div className='settings'>
+                                <Dropdown>
+                                    <Dropdown.Toggle className='btn-light btn-circle btn-sm' >
+                                    <BsFillGearFill size='27px' ></BsFillGearFill>
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item href="#/action-1">Send feedback</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">Help</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3">Settings</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </div>
+                            <div className='profile'>
+                                <Button className='btn-light btn-circle btn-sm'>
+                                    <BsPerson size='sm'></BsPerson>
+                                </Button> </div>
+                        </Form>
+                    </div>
+                </Navbar>
+               
+            </>
         )
     }
 }
