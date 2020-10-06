8757121d5a2377f8411dac778cc7b6216f8e7caa
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-07 04:02:56 +0530

    refactor(PinIcon): Add pin icon next to title bar

--- src/Components/CreateNotes.jsx
+++ src/Components/CreateNotes.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
 import { Accordion, Button, } from 'react-bootstrap'
+import {VscPinned} from 'react-icons/vsc'
 import "../styles/CreateNote.scss"
 
 export default function CreateNotes() {
     return (
         <Card className='createNotes'>
-        
-            
             <div className='inputFields'>
                 <div className='titleField'>
                     <input
                         placeholder='Title'
                         className='title' />
+                       
                     <textarea
                         name="content"
                         placeholder="Take a note..."
                         className='title'
                     />
-
+                    <button className='pinIcon'><VscPinned className='pin' size='22px'></VscPinned> </button>     
                 </div>
+               
             </div>
-            
+             
       
         </Card>
     );
