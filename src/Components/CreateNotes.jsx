61266acd03b62080c243e22ef1f5df9981690080
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-07 02:20:10 +0530

    refactor(ClassNames): Add className to follow style for different div

--- src/Components/CreateNotes.jsx
+++ src/Components/CreateNotes.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
-import { Accordion, Button,} from 'react-bootstrap'
+import { Accordion, Button, } from 'react-bootstrap'
 import "../styles/CreateNote.scss"
 
 export default function CreateNotes() {
     return (
-        <div className='createNotes' >
-            <Accordion defaultActiveKey="0" className='inputs'>
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                           <input type="text" className='inputText'placeholder='Title' />
-                        </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="0">
-                        <Card.Body><input type="text" /></Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
+        <Card className='createNotes'>
+        
+            
+            <div className='inputFields'>
+                <div className='titleField'>
+                    <input
+                        placeholder='Title'
+                        className='title' />
+                    <textarea
+                        name="content"
+                        placeholder="Take a note..."
+                        className='title'
+                    />
 
-        </div>
-    )
+                </div>
+            </div>
+            
+      
+        </Card>
+    );
+    
 }
