741df9f97b16e9857e009d2f2d51516bda87a873
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-07 05:13:48 +0530

    style(ButtonIcons): Add icons in card to perform operations

--- src/Components/CreateNotes.jsx
+++ src/Components/CreateNotes.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
 import { Accordion, Button, } from 'react-bootstrap'
-import {VscPinned} from 'react-icons/vsc'
+import {VscPinned,VscSymbolColor} from 'react-icons/vsc'
+import {MdAddAlert,MdMoreVert} from 'react-icons/md';
+import {RiUserAddFill,RiInboxArchiveLine} from 'react-icons/ri'
+import {BiImageAlt} from 'react-icons/bi'
 import "../styles/CreateNote.scss"
 
 export default function CreateNotes() {
@@ -10,18 +13,30 @@ export default function CreateNotes() {
             <div className='inputFields'>
                 <div className='titleField'>
                     <input
-                        placeholder='Title'
+                        placeholder=' Title'
                         className='title' />
                        
                     <textarea
                         name="content"
-                        placeholder="Take a note..."
+                        placeholder=" Take a note..."
                         className='title'
                     />
                     <button className='pinIcon'><VscPinned className='pin' size='22px'></VscPinned> </button>     
                 </div>
                
             </div>
+            <div className='iconsOption'>
+                <button className='buttons'><MdAddAlert ></MdAddAlert></button>
+                <button className='buttons'><RiUserAddFill></RiUserAddFill></button>
+                <button className='buttons'><VscSymbolColor></VscSymbolColor></button>
+                <button className='buttons'><BiImageAlt></BiImageAlt></button>
+                <button className='buttons'><RiInboxArchiveLine></RiInboxArchiveLine></button>
+                <button className='buttons'><MdMoreVert></MdMoreVert></button>
+
+                <button className='closeButton'>Close</button>
+
+
+                </div>
              
       
         </Card>
