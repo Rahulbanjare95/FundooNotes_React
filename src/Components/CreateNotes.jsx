3d21dfd91292562d999a7f198df06725ed573afe
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-08 09:34:26 +0530

    feat(ApiCall): Post new note by making a POST request via Notes api

--- src/Components/CreateNotes.jsx
+++ src/Components/CreateNotes.jsx
@@ -1,59 +1,92 @@
 import React, { useState } from 'react';
-import Card from 'react-bootstrap/Card'
-import { Accordion, Button, } from 'react-bootstrap'
+import {Card, Dropdown} from 'react-bootstrap';
 import { VscPinned, VscSymbolColor } from 'react-icons/vsc'
 import { MdAddAlert, MdMoreVert } from 'react-icons/md';
 import { RiUserAddFill, RiInboxArchiveLine } from 'react-icons/ri'
 import { BiImageAlt } from 'react-icons/bi'
 import "../styles/CreateNote.scss"
-import { TextField } from '@material-ui/core';
+import NotesServices from "../services/NotesServices"
+import { CodeSharp } from '@material-ui/icons';
 
 export default function CreateNotes() {
     const [titleFieldVisible, setTitleFieldVisible] = useState(false);
+    const [note, setNote] = useState({ title: "", description: "" });
+    const onChangeUser = (e) => { 
+        setNote({ ...note, [e.target.name]: e.target.value })
+        console.log(note);
+    }
     const showTitleField = () => {
         setTitleFieldVisible(true)
     }
 
-    const hideTitleField = ()  => {
+    const hideTitleField = () => {
         setTitleFieldVisible(false)
-      }
+    }
+    const onSubmitSaveNote = (e) => {
+        NotesServices.createNew(note);
+        e.preventDefault();
+        console.log(note);
+    };
 
     return (
         <Card className='createNotes'>
-            <div className='inputFields'>
             {titleFieldVisible && (
-              <div className="backdrop" onClick={hideTitleField} />
-            )}
-                <div className='titleField'>
-                    {titleFieldVisible && (<input
-                        placeholder=' Title'
-                        className='title'
-                        onFocus={showTitleField}
-                       />)}
-                    
-
-                    <textarea
-                        name="content"
-                        placeholder=" Take a note..."
-                        className='title'
-                        onFocus={showTitleField} 
-
-                    />
-                    <button className='pinIcon'><VscPinned className='pin' size='22px'></VscPinned> </button>
-                {titleFieldVisible && (< div className='iconsOption'>
-                <button className='buttons'><MdAddAlert ></MdAddAlert></button>
-                <button className='buttons'><RiUserAddFill></RiUserAddFill></button>
-                <button className='buttons'><VscSymbolColor></VscSymbolColor></button>
-                <button className='buttons'><BiImageAlt></BiImageAlt></button>
-                <button className='buttons'><RiInboxArchiveLine></RiInboxArchiveLine></button>
-                <button className='buttons'><MdMoreVert></MdMoreVert></button>
-                <button className='closeButton'>Close</button>
-            </div>)}
-                
+                        <div className="backdrop" onClick={hideTitleField} />
+                    )}
+                <div className='inputFields'>
+               
+                <form  onSubmit={onSubmitSaveNote}  className='titleField'>
+                   
+                    <div>
+                        
+                        {titleFieldVisible && (<span   
+                            onClick={showTitleField}
+                        />)}
+                        <input
+                            type='text'
+                            placeholder=' Title'
+                            className='title'
+                            name='title'
+                            value={note.title}
+                            onChange={onChangeUser}
+                            
+                        />
+                        <textarea
+                            type='text'
+                            placeholder=" Take a note..."
+                            rows={titleFieldVisible ? "3" : "1"}
+                            className='title'
+                            name='description'
+                            value={note.description}
+                            onFocus={showTitleField}
+                            onChange={onChangeUser}
+                        />
+                        <button className='pinIcon'><VscPinned className='pin' size='22px'></VscPinned> </button>
+                        {titleFieldVisible && (< div className='iconsOption'>
+                            <button className='buttons'><MdAddAlert ></MdAddAlert></button>
+                            <button className='buttons'><RiUserAddFill></RiUserAddFill></button>
+                            <button className='buttons'><VscSymbolColor></VscSymbolColor></button>
+                            <button className='buttons'><BiImageAlt></BiImageAlt></button>
+                            <button className='buttons'><RiInboxArchiveLine></RiInboxArchiveLine></button>
+                            <button className='buttons'>
+                            <Dropdown>
+                                    <Dropdown.Toggle className='btn-light buttons' >
+                                    <MdMoreVert></MdMoreVert>
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item href="#/action-1">Add laber</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">Add Drawing</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3">Show Checkbox</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                </button>
+                            <button className='closeButton' type="submit">Close</button>
+                        </div>)}
+                      
+                    </div>
+                    </form>
                 </div>
-
-            </div>
-            
+          
         </Card>
     );
 
