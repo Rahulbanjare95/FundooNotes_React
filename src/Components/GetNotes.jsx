8e1f0b6cf184532de73b3b1e619dc9d4134e1ff0
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-09 07:17:25 +0530

    feat(Display): Create all cards from the api response

--- src/Components/GetNotes.jsx
+++ src/Components/GetNotes.jsx
@@ -1,26 +1,37 @@
 import React, { Component } from 'react'
-
+import "../styles/CreateNote.scss"
 export default class GetNotes extends Component {
     state = {
-        note:null
+        note: null
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         const token = localStorage.getItem("token");
         const URL = "http://fundoonotes.incubation.bridgelabz.com/api/notes/";
         const url = `${URL}getNotesList?access_token=${token}`;
-        const response = await fetch(url);
-        const data  = await response.json();
-        this.setState({note: data.data})
-        console.log(this.state.note.data.title);
-
+        fetch(url).then(response => {
+            return response.json();
+        }).then(data => {
+            this.setState({ note: data.data.data });
+            console.log(this.state.note);
+        }).catch(error => console.log(error));
     }
+
     render() {
         return (
-            <div>
-                {this.state.note ? (<div>Yes Notes are here</div>)
-                :
-                ( <div>No notes found</div>)}
+
+            <div className='noteList'>
+                {this.state.note ? (
+                    <div>
+                        {this.state.note.map((data, index) =>{
+                            return <div ><div className='note'><h1> {data.title}</h1>
+                                                                <p>{data.description}</p></div> </div>
+                            
+                        })}
+                    </div>)
+                    :
+                    (<div>No notes found</div>)
+                }
             </div>
         )
     }
