7b57765d4ea43bf1501dd64127c1746587f19b28
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-11 08:20:44 +0530

    feat(GetNotes): Show cards only which are not in trash

--- src/Components/GetNotes.jsx
+++ src/Components/GetNotes.jsx
@@ -1,9 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Spinner from 'react-bootstrap/Spinner'
+import { VscSymbolColor } from 'react-icons/vsc'
+import { BiTrashAlt } from 'react-icons/bi';
+import { MdAddAlert } from 'react-icons/md';
+import { RiUserAddFill, RiInboxArchiveLine } from 'react-icons/ri'
+import { BiImageAlt } from 'react-icons/bi'
+import '../styles/CardIcons.scss'
 import "../styles/CreateNote.scss"
+import notesServices from '../services/NotesServices'
+import CardIcons from './CardIcons';
+import { ControlCamera } from '@material-ui/icons';
 export default class GetNotes extends Component {
     state = {
-        note: null
+        note: null,
+        id: null,
+        isDeleted: false
     }
 
     async componentDidMount() {
@@ -14,21 +25,39 @@ export default class GetNotes extends Component {
             return response.json();
         }).then(data => {
             this.setState({ note: data.data.data });
-            console.log(this.state.note);
         }).catch(error => console.log(error));
     }
 
+    deleteNote(data) {
+        // console.log(data);
+        var noteData = { noteIdList: [data.id], isDeleted: true }
+        notesServices.deleteNote(noteData);
+    }
     render() {
         return (
-
             <div className='noteList'>
                 {this.state.note ? (
                     <div>
-                        {this.state.note.map((data, index) =>{
-                            return  <div className='note'><h1> {data.title}</h1>
-                                                                <p>{data.description}</p>
-                                                                </div>
-                            
+
+                        {this.state.note.filter((data) => !data.isDeleted).map((data, index) => {
+                            const id = data.id;
+                            console.log('this is isdeleted'+ data.isDeleted);
+
+                                return (
+                                
+                                    <div className='note' key={index}><h1>{data.title}</h1>
+                                        <p>{data.description}</p>
+                                        <p>{String(data.isDeleted)}</p>
+                                        <div className='iconsDiv'>
+                                            <button className='iconsCard'><MdAddAlert ></MdAddAlert></button>
+                                            <button className='iconsCard'><RiUserAddFill></RiUserAddFill></button>
+                                            <button className='iconsCard'><VscSymbolColor></VscSymbolColor></button>
+                                            <button className='iconsCard'><BiImageAlt></BiImageAlt></button>
+                                            <button className='iconsCard'><RiInboxArchiveLine></RiInboxArchiveLine></button>
+                                            <button className='iconsCard' onClick={() => this.deleteNote({ id })}><BiTrashAlt></BiTrashAlt></button>
+                                        </div>
+                                    </div>)
+                           
                         })}
                     </div>)
                     :
