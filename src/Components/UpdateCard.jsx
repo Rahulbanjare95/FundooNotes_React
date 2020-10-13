5655d9b94687d4d696d25db3da51ebfe10f8d9bd
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-13 12:08:17 +0530

    refactor: Receive props from parent component on card clicks

--- src/Components/UpdateCard.jsx
+++ src/Components/UpdateCard.jsx
@@ -1,61 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {Modal, Button} from 'react-bootstrap';
+import NotesServices from '../services/NotesServices'
 
- class UpdateCard extends Component {
-    constructor(props){
-      console.log( 'I am inside UpdateCard',props);
-        super(props);
-         this.state ={
-            showModal:true,
-        }
-        // this.titleHandler = this.titleHandler.bind(this);
-        // this.descriptionHandler =this.descriptionHandler.bind(this);
+class UpdateCard extends Component {
+  constructor(props){
+    super(props);
+    console.log("I am in Props",props);
+    this.state = {
+      title: '',
+      description:'',
     }
+  }
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      title: nextProps.title,
+      description: nextProps.description,
+      });
+  }
 
-    componentWillReceiveProps(nextProps){
-        this.setState({
-            title: nextProps.title,
-            description: nextProps.description
-        });
-    }
-    handleClose = () =>{
-      this.setState({showModal:false});
-    } 
+  titleHandler(e){
+    this.setState({title: e.target.value});
+  }
 
-
-    titleHandler = (e) =>{
-        this.setState({title: e.target.value});
-    }
-
-    descriptionHandler = (e) =>{
-        this.setState({description: e.target.value});
-    }
-
-    handleSave=(e)=>{
-        const note = {}
-        note.id  = this.props.id;
-        note.title = this.props.title;
-        note.description = this.props.description;
-    }
-    
-    render(){
-        return (
-            <Modal show={this.props.showModal}>
-        <Modal.Header>
-          <Modal.Title>Update </Modal.Title>
-        </Modal.Header>
-        <Modal.Body> <input value={this.props.title} onChange={(e) => this.titleHandler(e)} />
-        <input value={this.props.description} onChange={(e) => this.descriptionHandler(e)} /></Modal.Body>
+  descriptionHandler(e){
+    this.setState({description: e.target.value});
+  }
+  
+  handleSave(e){
+    var updateNote = { noteIdList: [this.props.id], title:this.state.title, description:this.state.description};
+    NotesServices.updateNote(updateNote);
+    console.log('hi',updateNote);
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={() => this.handleSave}>
+        <Modal show={this.props.show} onHide={this.props.close}>
+        <Modal.Body> <span> <input value={this.state.title} onChange={(e) => this.titleHandler(e)}/></span>
+        <br/>
+        <span> <input value={this.state.description} onChange={ (e) => this.descriptionHandler(e)}/></span>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={()=> {this.handleClose()}}>
+          <Button variant="secondary" onClick={this.props.close}>
             Close
           </Button>
-          <Button variant="primary" onClick={() => { this.handleSave() }}>
+          <Button variant="primary" type="submit">
             Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
-        );
-    }
+      </form>
+      </div>
+    )
+  }
 }
+
 export default UpdateCard;
