10f4a11349ee2333f2ecdf3236dea5174058e6ff
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-15 11:13:16 +0530

    feat(Trash): Add deleted notes to trash

---
+++ src/Components/Trash.jsx
@@ -0,0 +1,63 @@
+import React, { Component, useState } from 'react'
+import Spinner from 'react-bootstrap/Spinner'
+import { FaRegTrashAlt } from 'react-icons/fa'
+import '../styles/CardIcons.scss'
+import "../styles/CreateNote.scss"
+import notesServices from '../services/NotesServices'
+export default class Archive extends Component {
+    state = {
+        note: null,
+        isArchived: false,
+        title: '',
+        description: '',
+    }
+
+    async componentDidMount() {
+        const token = localStorage.getItem("token");
+        const URL = "http://fundoonotes.incubation.bridgelabz.com/api/notes/";
+        const url = `${URL}getTrashNotesList?access_token=${token}`;
+        fetch(url).then(response => {
+            return response.json();
+        }).then(data => {
+            this.setState({ note: data.data.data });
+        }).catch(error => console.log(error))
+            ;
+    }
+    deleteForever(data) {
+        notesServices.deleteForever(data);
+
+    }
+
+    render() {``
+
+        return (
+            <div className='noteList'>
+                {this.state.note ? (
+
+                    <div>
+                        {this.state.note.map((data, index) => {
+                            const id = data.id;
+                            console.log('this is id', id);
+                            return (
+                                <>
+                                    <div className='note'>
+                                        <h1>{data.title}</h1>
+                                        <p>{data.description}</p>
+                                        <div className='iconsDiv'>
+
+                                            <button className='iconsCard' onClick={() => this.deleteForever({ id })}><FaRegTrashAlt></FaRegTrashAlt></button>
+
+                                        </div>
+                                    </div>
+                                </>
+                            )
+                        })}
+
+                    </div>)
+                    :
+                    (<div className='noteList'> <Spinner animation="border" variant="warning" /> </div>)
+                }
+            </div>
+        )
+    }
+}
