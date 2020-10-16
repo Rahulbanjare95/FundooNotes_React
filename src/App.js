d27dde18c2b2547cbf4629d3237d328efd5dcb29
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-16 12:44:37 +0530

    refactor(App): Remov old components and renders only Routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
 import Register from './Components/Register';
 import './App.css';
-import SignIn from './Components/SignIn';
-import ForgetPassword from './Components/ForgetPassword';
-import ResetPassword from './Components/ResetPassword';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Dashboard from './Components/Dashboard';
-import Archive from './Components/Archive';
+import Routing from './router';
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          
-          <Route path="/" exact component={SignIn} ></Route>
-          <Route path="/register" component={Register} />
-          <Route path="/SignIn"  component={SignIn} />
-          <Route path="/ForgetPassword"  component={ForgetPassword} />
-          <Route path="/resetpassword/:token"  component={ResetPassword} />
-          <Route path="/Dashboard" component={Dashboard} />
-      
-          <Route path={'/archive'} component={Archive}>
-
-          </Route>
-
-        </Switch>
-
-      </Router>
+      <div>
+        <Routing />
+        </div>
     )
   }
 }
