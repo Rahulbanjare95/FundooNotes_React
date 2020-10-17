15f782374469c253922eade364c02f57093b387a
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-17 11:25:08 +0530

    refactor(Search): Search bar moved to separate div

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,62 +1,68 @@
 import React from "react";
-import { Navbar, Button, Form, FormControl, Dropdown,} from 'react-bootstrap';
+import { Navbar, Button, Form, Dropdown } from 'react-bootstrap';
 import { Component } from 'react';
 import download from '../Assets/download.jpg';
 import "../styles/Navbar.scss";
-import { BsArrowClockwise, BsFillGearFill, BsPerson } from "react-icons/bs";
+import "../styles/SearchBar.scss";
+import { BsArrowClockwise, BsFillGearFill, BsPerson,BsSearch } from "react-icons/bs";
 import DrawerToggleButton from "./DrawerToggleButton";
 import SideBar from "./SideBar";
 import Backdrop from "./Backdrop";
-import CreateNotes from "./CreateNotes";
-import GetNotes from "./GetNotes";
 import Profile from './Profile';
 
 class Dashboard extends Component {
     state = {
-        sideDrawerOpen:false,
-        onCLickCall:false,
-        openProfile:false,
+        sideDrawerOpen: false,
+        onCLickCall: false,
+        openProfile: false,
 
 
     };
 
-    drawerToggleClickHandler = () =>{
+    drawerToggleClickHandler = () => {
         this.setState((previousState) => {
-            return{sideDrawerOpen: !previousState.sideDrawerOpen};
+            return { sideDrawerOpen: !previousState.sideDrawerOpen };
         });
     };
-    backdropClickHandler = () =>{
-        this.setState({sideDrawerOpen:false});
+    backdropClickHandler = () => {
+        this.setState({ sideDrawerOpen: false });
     }
-    showProfile = () =>{
-        this.setState({openProfile:true});
+    showProfile = () => {
+        this.setState({ openProfile: true });
 
     }
 
-    render() {    
+    render() {
 
         let sideDrawer;
         let backDrop;
-        if(this.state.sideDrawerOpen){
-            sideDrawer = <SideBar/>;
-            backDrop = <Backdrop  click = {this.backdropClickHandler}/>;
+        if (this.state.sideDrawerOpen) {
+            sideDrawer = <SideBar />;
+            backDrop = <Backdrop click={this.backdropClickHandler} />;
         }
         return (
             <>
                 <Navbar variant="default" className="custom-nav-bar" >
                     <div>
-                    <DrawerToggleButton  handleToggleClick={this.drawerToggleClickHandler}/>
-                  
-                    {backDrop}
+                        <DrawerToggleButton handleToggleClick={this.drawerToggleClickHandler} />
+
+                        {backDrop}
                     </div>
                     <Navbar.Brand><img
                         src={download}
                         alt="Imge"
                         className="custom-image" /><span className="custom-brand">FundooNotes</span></Navbar.Brand>
 
+                    <div className='searchBar'>
+                        <button type="submit" className='searchButton'><BsSearch></BsSearch></button>
+                        <input type="text" className='inputSearchBar'  placeholder='Search'>
+                        
+                       </input>
+                    </div>
                     <div >
-                        <Form inline >
-                            <FormControl type="text" placeholder="Search" className="searchBar" />
+
+                        <Form inline>
+
                             <div className='refresh'>
                                 <Button className='btn-light btn-circle btn-sm'>
                                     <BsArrowClockwise size='20px'> </BsArrowClockwise>
@@ -65,7 +71,7 @@ class Dashboard extends Component {
                             <div className='settings'>
                                 <Dropdown>
                                     <Dropdown.Toggle className='btn-light btn-circle btn-sm' >
-                                    <BsFillGearFill size='20px' ></BsFillGearFill>
+                                        <BsFillGearFill size='20px' ></BsFillGearFill>
                                     </Dropdown.Toggle>
                                     <Dropdown.Menu>
                                         <Dropdown.Item href="#/action-1">Send feedback</Dropdown.Item>
@@ -76,18 +82,17 @@ class Dashboard extends Component {
                             </div>
                             <div className='profile'>
                                 <Button className='btn-light btn-circle btn-sm'>
-                                    <BsPerson size='20px' onClick={this.showProfile?<Profile/>: null}> </BsPerson>    
+                                    <BsPerson size='20px' onClick={this.showProfile ? <Profile /> : null}> </BsPerson>
                                 </Button> </div>
                         </Form>
                     </div>
                 </Navbar>
-                
                 <div className='contents'>
-                      <SideBar/> 
+                    <SideBar />
 
                 </div>
-                
-               
+
+
             </>
         )
     }
