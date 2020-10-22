ae72d62c50eb594a7975929727c4ef045cb86b87
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-22 09:41:12 +0530

    feat(labels): User labels are viewed inside sidebar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component, useEffe } from 'react';
+import axios from 'axios';
 import "../styles/SideBar.scss";
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { BiBulb, BiArchiveIn, BiTrashAlt } from 'react-icons/bi';
 import { BsPencil, BsBell } from 'react-icons/bs';
+import { MdLabelOutline } from 'react-icons/md';
 
-const sideBar = props => {
-    return (
-        <div>
+
+class SideBar extends Component {
+    state = {
+        label: [],
+    }
+
+
+    async componentDidMount() {
+        const token = localStorage.getItem("token");
+        const URL = "http://fundoonotes.incubation.bridgelabz.com/api/noteLabels/";
+        const url = `${URL}getNoteLabelList?access_token=${token}`;
+        fetch(url).then(response => {
+            return response.json();
+        }).then(data => {
+            this.setState({ label: data.data.details });
+            console.log(data);
+            console.log(this.state.label);
+        }).catch(error => console.log(error));
+    }
+
+    render() {
+        //    const { data } = this.state.label;
+        return (<div>
             <nav className="side-drawer">
+
+
                 <ul className="side-drawer-ul">
                     <li className="side-drawer-item">
                         <Link to='/dashboard/notes' className="side-drawer-link" activeClassName="active">  <BiBulb className="icons"></BiBulb>
@@ -19,12 +43,25 @@ const sideBar = props => {
                     <li>
                         <a href="/#" className="side-drawer-link">
                             <BsBell className="icons"></BsBell>
-                            <span className="side-drawer-text"> Reminders  </span></a></li>
-                    <li>
-                        <Link to='/dashboard/archive' className="side-drawer-link" activeClassName="active">  <BiArchiveIn className="icons"></BiArchiveIn>
-
-                            <span className="side-drawer-text">Archive </span></Link>
+                            <span className="side-drawer-text"> Reminders  </span></a>
                     </li>
+
+                    <li>
+                        <NavLink to='/dashboard/archive' className="side-drawer-link" activeClassName="active">  <BiArchiveIn className="icons"></BiArchiveIn>
+
+                            <span className="side-drawer-text">Archive </span></NavLink>
+                    </li>
+                    {this.state.label.map((label) => {
+                        console.log(label);
+                        return (
+                            <li>
+                                <div className='label'>
+                                    <NavLink to='' className="side-drawer-link" activeClassName="active">  <MdLabelOutline className="icons"></MdLabelOutline>
+                                        <span className="side-drawer-text">{label.label} </span></NavLink>
+                                </div>
+                            </li>
+                        );
+                    })}
 
                     <li>
                         <Link to='/dashboard/trash' className="side-drawer-link" activeClassName="active">  <BiTrashAlt className="icons"></BiTrashAlt>
@@ -33,11 +70,18 @@ const sideBar = props => {
                     </li>
 
                 </ul>
+
             </nav >
         </div>
 
-    );
+        );
+    }
+}
 
-};
+// const sideBar = props => {
+//     return (
 
-export default sideBar;
+
+// };
+
+export default SideBar;
