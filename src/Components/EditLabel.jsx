3601f8ed0c4c5c5ec8ebaa10931ed81ce81b2fe0
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-24 10:19:52 +0530

    feat(Edit): Edit existing labels onclick of their name

--- src/Components/EditLabel.jsx
+++ src/Components/EditLabel.jsx
@@ -4,9 +4,8 @@ import labelServices from '../services/LabelServices';
 import axios from 'axios';
 import { IoMdAdd } from 'react-icons/io';
 import { ImCancelCircle } from 'react-icons/im'
-import {MdDelete} from 'react-icons/md';
+import { MdDelete, MdModeEdit } from 'react-icons/md';
 import '../styles/CardIcons.scss'
-import "../styles/CreateNote.scss"
 import '../styles/LabelStyle.scss'
 
 const token = localStorage.getItem("token");
@@ -15,7 +14,7 @@ export default class EditLabel extends Component {
         super(props);
         this.state = {
             label: [],
-            isDeleted: 'false',
+            isDeleted: false,
             show: false,
             newLabelName: '',
         }
@@ -30,34 +29,23 @@ export default class EditLabel extends Component {
         fetch(url).then(response => {
             return response.json();
         }).then(data => {
-            this.setState({ label: data.data.details, isDeleted: data.data.details.isDeleted });
-            console.log('Edit label Mount', this.state.label);
-            console.log('label array label', this.label.id)
+            this.setState({ label: data.data.details, isDeleted: data.data.details.isDeleted, newLabelName: data.data.details.label });
         }).catch(error => console.log(error));
     }
-
     handleModal() {
         this.setState({ show: true });
     }
 
-    labelHandler(e) {
-        this.setState({ label: e.target.value });
-    }
-
-    handleSave() {
-        var Label = { noteId: this.props.id, la: this.state.label, isdeleted: false };
+    labelHandler(e, index) {
+        this.state.label[index].label = e.target.value;
+        this.setState({ label: this.state.label });
     }
 
     newLabelNameHandler(e) {
         this.setState({ newLabelName: e.target.value });
-        this.componentDidMount();
     }
     deletelabel(data) {
-        var noteData = { id:data};
-        // labelServices.deleteLabel(noteData);
-        
-        axios.post(`http://fundoonotes.incubation.bridgelabz.com/api/noteLabels/${noteData}/deleteNoteLabel?access_token=${token}`,data);
-        this.componentDidMount();
+        axios.delete(`http://fundoonotes.incubation.bridgelabz.com/api/noteLabels/${data.id}/deleteNoteLabel?access_token=${token}`);
     }
     createLabel() {
         var data = {
@@ -66,32 +54,32 @@ export default class EditLabel extends Component {
             userId: localStorage.getItem('userId')
         }
         axios.post(`http://fundoonotes.incubation.bridgelabz.com/api/noteLabels?access_token=${token}`, data);
+        this.setState({ newLabelName: '' });
     }
 
-
+    editLabel = (labelId, index) => {
+        console.log('this is labelId', labelId);
+        let editdata = { label: this.state.label[index].label };
+        axios.post(`http://fundoonotes.incubation.bridgelabz.com/api/noteLabels/${labelId}/updateNoteLabel?access_token=${token}`, editdata);
+    };
     render() {
         return (
-
-            <Modal show={this.props.show} onHide={this.props.close} dialogClassName={"createNotes"}  >
-                <div className='createNewLabel'>
+            <Modal show={this.props.show} onHide={this.props.close}  className='createNewLabel'  >
+                <Modal.Header>Edit Label</Modal.Header>
+                <div  className='createNewLabel'  >
                     <button className='iconsCard'><ImCancelCircle /></button>
                     <input type="text" placeholder="Create new label" className='inputLabel' value={this.state.newLabelName} onChange={(e) => { this.newLabelNameHandler(e) }} required />
                     <button className='iconsCard' onClick={this.createLabel}><IoMdAdd /></button>
                 </div>
-                {this.state.label.map((label) => {
-                    console.log('LAbel inside Edit labe map', label);
-                    const id = label.id;
+                {this.state.label.filter((data) => !data.isDeleted).map((data, index) => {
+                    const id = data.id;
                     return (
-                        <Modal.Body >
-                            <button className='iconsCard'onClick={() => this.deletelabel({id})}><MdDelete/> </button>
-                            <input value={label.label} onChange={(e) => { this.labelHandler() }} />
-                        </Modal.Body>)
+                        <div className='createNewLabel' >
+                            <button className='iconsCard ' onClick={() => this.deletelabel({ id })}><MdDelete /> </button>
+                            <input  className='inputLabel' value={data.label} onChange={(e) => this.labelHandler(e, index)} />
+                            <button className='iconsCard' onClick={(e) => this.editLabel(data.id, index)}><MdModeEdit /></button>
+                        </div>)
                 })}
-
-                <Modal.Footer>
-                    <button onClick={this.props.close}>Cancel</button>
-                    <button>Save</button>
-                </Modal.Footer>
             </Modal>
 
         );
